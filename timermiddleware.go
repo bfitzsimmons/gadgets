6503d530fef6103b982c4609227885952ce4d427
Brant Fitzsimmons <brant.fitzsimmons@gmail.com>
2017-03-13 14:11:26 -0400

    Added a timer.

---
+++ timermiddleware.go
@@ -0,0 +1,25 @@
+package gadgets
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+// TimerMiddleware times the request/response time.
+func TimerMiddleware(next http.Handler) http.Handler {
+	timerFunc := func(w http.ResponseWriter, r *http.Request) {
+		// Start the timer.
+		startTime := time.Now()
+
+		// Run the other middleware/handlers.
+		next.ServeHTTP(w, r)
+
+		// Print the execution time.
+		go func() {
+			log.Println(time.Since(startTime))
+		}()
+	}
+
+	return http.HandlerFunc(timerFunc)
+}
