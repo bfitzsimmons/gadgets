8fadb35d5cea5901d9d525be0a4a62c0659008ec
Brant Fitzsimmons <brant.fitzsimmons@gmail.com>
2018-09-13 16:55:44 -0400

    Modified the slugify function to be a little more precise, and added tests.
    Always write tests!!!

--- text.go
+++ text.go
@@ -8,17 +8,18 @@ import (
 // Slugify takes an input string, slugifies (remove special characters, replaces spaces with "-", etc.) it, and returns
 // it as a string.
 func Slugify(input string) string {
-	// Trim the input string.
-	input = strings.TrimSpace(input)
-
 	// Make lower case.
 	input = strings.ToLower(input)
 
-	// Remove non-alphanumeric characters and hyphens.
-	input = regexp.MustCompile("[^a-z0-9- ]").ReplaceAllString(input, "")
+	// Deal with hyphens and underscores.
+	input = strings.Replace(input, "-", " ", -1)
+	input = strings.Replace(input, "_", " ", -1)
+
+	// Remove non-alphanumeric characters.
+	input = regexp.MustCompile(`[^a-z0-9 ]`).ReplaceAllString(input, "")
 
 	// Replace spaces with "-".
-	input = regexp.MustCompile("[\\s]+").ReplaceAllString(input, "-")
+	input = strings.Join(strings.Fields(input), "-")
 
 	return input
 }
